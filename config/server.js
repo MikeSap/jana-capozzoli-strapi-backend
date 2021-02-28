129b4f48ae3a6b9fa1f1c24e37e676ee60fa8b86
mike <mikesapienzaaudio@gmail.com>
2021-02-28 14:08:22 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df24e575ba1c7efb14fd72e4490d3898'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfb209db-bfe4-44ed-a893-2861aa322d80'
+};
